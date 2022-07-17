022b19b097b099108dd85c5a7c603c94a19c28b6
Yulia <yudemins@mail.ru>
2022-07-17 18:53:55 +0300

    complite task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,45 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  let res = [];
+
+  let resNo = []
+  const brackets = {
+    ')': '(',
+    '}': '{',
+    ']': '[',
+    '2': '1',
+    '4': '3',
+    '6': '5',
+  }
+
+let arr = str.split('');
+const Close = (value) => Object.keys(brackets).indexOf(value) >= 0;
+const bracket = (value) => {
+  return (Object.keys(brackets).includes(value))||(Object.values(brackets).includes(value))
+}
+
+for (let i = 0; i < arr.length; i++) {
+  
+  if (!bracket(arr[i])) {
+    if (resNo[resNo.length - 1] === arr[i]) {
+      resNo.pop()
+      res.pop()
+    }
+    else {
+      resNo.push(arr[i])
+      res.push(arr[i])
+    }    
+ }
+
+  else {
+    if (Close(arr[i])) {
+      if (brackets[arr[i]] !== res.pop()) return false;
+    } else {
+      res.push(arr[i])
+    }
+  }
+}
+
+return res.length == 0 && resNo.length == 0
+
 }
